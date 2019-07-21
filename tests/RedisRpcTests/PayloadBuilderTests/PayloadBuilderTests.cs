5bc8afdd8176b3fd918269a92a0b3e566962d5bb
Milovidov Vasya <milovidov.vasya@gmail.com>
2019-07-21 16:08:26 +0300

    add tests

--- tests/RedisRpcTests/PayloadBuilderTests/PayloadBuilderTests.cs
+++ tests/RedisRpcTests/PayloadBuilderTests/PayloadBuilderTests.cs
@@ -1,16 +1,39 @@
-using System;
-using Xunit;
 using RedisRpc.Builders;
-using RedisRpc.Interfaces;
+using RedisRpcTests.CommonExtentions;
+using System.Linq;
+using System.Text;
+using Xunit;
 
 namespace RedisRpcTests {
 	public class PayloadBuilderTests {
 		[Fact]
 		public void SetOneStream_ReturneOneStream() {
+			var testMessage = "Test message".ToStream();
 
+			var payload = new PayloadBuilder()
+				.WithStream(testMessage)
+				.Build();
 
-			var builder = new PayloadBuilder();
-
+			Assert.Single(payload.Offsets);
 		}
+
+		[Fact]
+		public void SetOneStream_ReturneOneProperStream() {
+			var testMessage1 = "Test message 1";
+			var testMessage2 = "Test message 2";
+
+
+			var payload = new PayloadBuilder()
+				.WithStream(testMessage1.ToStream())
+				.WithStream(testMessage2.ToStream())
+				.Build();
+
+			var first = payload.Body.Take(payload.Offsets.First()).ToArray();
+			var result = Encoding.UTF8.GetString(first);
+
+
+			Assert.Equal(testMessage1, result);
+		}
+
 	}
 }
